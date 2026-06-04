2a23411385c5b7c72d646db52c10a33f3407ef47
Hjeandedieu <hashimweyesujeandedie@gmail.com>
2026-06-04 10:32:49 +0200

    Added Error Handling

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 import express from "express";
 
+// Routes
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 
+// Middleware
+import errorMiddleware from "./middleware/error.middleware.js";
+import cookieParser from "cookie-parser";
+
+// Database 
 import connectToDatabase from "./database/mongodb.js";
 
+// Port
 import { PORT } from "./config/env.js";
 
+
+
 const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser)
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware)
 
 app.get("/", (req, res) => {
   res.send("Welcome to the subscription tracker API!");
